04cb487ccff25c485056a311fdab69c586fb4132
rmt/src <144435+rmtsrc@users.noreply.github.com>
2025-05-21 15:26:32 +0100

    wallpaper-a-day 0.2.6

--- Casks/w/wallpaper-a-day.rb
+++ Casks/w/wallpaper-a-day.rb
@@ -1,6 +1,6 @@
 cask "wallpaper-a-day" do
-  version "0.2.5"
-  sha256 "48151e907a2ce0f19645b59235eaccfa368af8ff7d09e20fa913cdaec6f773d1"
+  version "0.2.6"
+  sha256 "5da3c128cc56ec5483efe8d7efdbff7dad7238664c0b19b67a0705f9eb6a450b"
 
   url "https://github.com/RobinNaumann/wallpaper_a_day/releases/download/v#{version}/Wallpaper.a.Day.app.zip",
       verified: "github.com/RobinNaumann/"
