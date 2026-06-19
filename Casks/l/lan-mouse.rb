a769b15a907618d63a25cc04ce0cb0b4a556afb4
rmt/src <144435+rmtsrc@users.noreply.github.com>
2026-06-19 09:39:26 +0100

    lan-mouse 0.11.0 (new cask)

---
+++ Casks/l/lan-mouse.rb
@@ -0,0 +1,29 @@
+cask "lan-mouse" do
+  version "0.11.0"
+  sha256 "5ff9965d05be7f125b1d75b9e7259d874ec98aa086ce40012522bc25406500a9"
+
+  url "https://github.com/feschber/lan-mouse/releases/download/v#{version}/lan-mouse-macos-arm64.zip"
+  name "Lan Mouse"
+  desc "Mouse & keyboard sharing via LAN"
+  homepage "https://github.com/feschber/lan-mouse"
+
+  depends_on macos: :catalina
+
+  app "Lan Mouse.app"
+
+  postflight do
+    system_command '/usr/bin/xattr', args: ['-r', '-d', 'com.apple.quarantine', "#{appdir}/Lan Mouse.app"]
+  end
+
+  caveats do
+    <<~EOS
+      This cask will remove the quarantine attribute from the installed application by running:
+        xattr -r -d com.apple.quarantine "#{appdir}/Lan Mouse.app"
+    EOS
+  end
+
+  zap trash: [
+    "~/.config/lan-mouse",
+    "~/Library/Preferences/de.feschber.LanMouse.plist",
+  ]
+end
